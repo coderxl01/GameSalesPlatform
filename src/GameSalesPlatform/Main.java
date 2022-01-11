1107e0c1a0b43fd3b29cbb5f6f8c95f9ccc5051b
kadirsnsy <kadirsnsy@isbak.istanbul>
2022-01-11 21:19:43 +0300

    service has been updated

--- src/GameSalesPlatform/Main.java
+++ src/GameSalesPlatform/Main.java
@@ -18,7 +18,7 @@ import Entities.Player;
 public class Main {
 
 	public static void main(String[] args) { 
-		Player player = new Player(1, "Kadir", "ÞENSOY", new Date(1993,05,16), "60199334386");
+		Player player = new Player(1, "FirstName", "LastName", new Date("Year","Month","Day"), "testtc");
         Game game = new Game(1, "PES 2020 PS4", 80,12);
         Campaign campaign = new Campaign(1,"Crazy Campaign",12);
         
